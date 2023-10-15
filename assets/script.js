29c9166007eb66c832117aa0171ed82ee6d011e9
chosta94 <tristanpoisson@orange.fr>
2023-10-15 16:40:17 +0200

    Ajout du changement de texte

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Tableau des images avec leurs taglines
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -15,28 +16,38 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
 
+// Sélection de tous les éléments dot
 const dot = document.querySelectorAll('.dot');
-const picture = document.querySelector('.banner-img');
-const listImg = ['./assets/images/slideshow/slide1.jpg', './assets/images/slideshow/slide2.jpg', './assets/images/slideshow/slide3.jpg', './assets/images/slideshow/slide4.png'];
+// Sélection de l'image principale du slider
+const picture = document.querySelector('.banner-img')
+// Sélection de la légende du slider
+const tagLine = document.querySelector('p');
+// Index initial pour le slide actif
 let index = 0;
 
+//Change l'image et la légende en fonction de l'index donné.
 function changeImage(index) {
-	picture.src = listImg[index];
+	picture.src = './assets/images/slideshow/' + slides[index].image;
+	tagLine.innerHTML = slides[index].tagLine;
   }
 
+//Met à jour l'indicateur dot actif en fonction de l'index.
 function changeDot(index) {
 	dot.forEach(dot => dot.classList.remove('dot_selected'));
 	dot[index].classList.add('dot_selected');
-}  
+}
 
+// Ajout d'un écouteur d'événements pour le bouton précédent
 document.getElementById('prev').addEventListener('click', () => {
+	// Si on est sur le premier slide, on va au dernier
 	if(index==0) {
 		index=3;
 		changeImage(index);
 		changeDot(index);
 	}
+	// Sinon, on va au slide précédent
 	else {
 		index = index - 1;
 		changeImage(index);
@@ -44,12 +55,15 @@ document.getElementById('prev').addEventListener('click', () => {
 	}
 });
 
+// Ajout d'un écouteur d'événements pour le bouton suivant
 document.getElementById('next').addEventListener('click', () => {
+	// Si on est sur le dernier slide, on retourne au premier
 	if(index==3) {
 		index=0;
 		changeImage(index);
 		changeDot(index);
 	}
+	// Sinon, on avance au slide suivant
 	else {
 		index = index + 1;
 		changeImage(index);
